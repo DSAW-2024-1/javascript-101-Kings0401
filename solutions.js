2ed0c6f361711cff0dac8d45845f5e69175e2661
Gabriela Reyes <gabrielarego@unisabana.edu.co>
2024-02-18 18:48:18 -0500

    Return 1

--- solutions.js
+++ solutions.js
@@ -16,7 +16,7 @@ function factorial(n) {
     console.log("Los datos de entrada no son adecuados");
     }
   if (n === 0 || n === 1){
-    return n===1;
+    return 1;
   }
   else{
     for (var i = n - 1; i >= 1; i--) {
