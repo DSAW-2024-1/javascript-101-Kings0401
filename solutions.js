2fdb8f6055c4ca4164ad53e7edb9a926cb25352a
Gabriela Reyes <gabrielarego@unisabana.edu.co>
2024-02-18 18:07:43 -0500

    Factorial

--- solutions.js
+++ solutions.js
@@ -1,11 +1,23 @@
 // Sum of Two Numbers
 function sum(a, b) {
   // Add your code here
+  if(typeof a !== "number" || typeof b !== "number"){
+    console.log("Los datos de entrada no son adecuados");
+    }
+    else{
+    return a+b;
+    }
 }
 
 // Factorial of a Number
 function factorial(n) {
   // Add your code here
+  if (numero === 0 || numero === 1)
+    return 1;
+  for (var i = numero - 1; i >= 1; i--) {
+    numero *= i;
+  }
+  return numero;
 }
 
 // Find the Largest Number
